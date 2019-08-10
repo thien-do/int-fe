b9c065d65f6a7b442eb64ebe8a8c6db0bcd7ed30
thien <dvkndn@gmail.com>
2019-08-10 15:55:09 +0700

    feat: improve server data

--- server/db.js
+++ server/db.js
@@ -1,29 +1,34 @@
 const faker = require("faker");
 
-const groupsCount = 10;
-const usersCount = 100;
+// Create 10 groups
+// https://blueprintjs.com/docs/#core/colors
+const groups = [
+  { "id": 0, "name": "Grocery", "color": "vermilion" },
+  { "id": 1, "name": "Home", "color": "rose" },
+  { "id": 2, "name": "Electronics", "color": "violet" },
+  { "id": 3, "name": "Kids", "color": "indigo" },
+  { "id": 4, "name": "Games", "color": "cobalt" },
+  { "id": 5, "name": "Beauty", "color": "turquoise" },
+  { "id": 6, "name": "Jewelery", "color": "forest" },
+  { "id": 7, "name": "Industrial", "color": "lime" },
+  { "id": 8, "name": "Tools", "color": "gold" },
+  { "id": 9, "name": "Clothing", "color": "sepia" },
+];
 
-module.exports = () => {
-  const data = { groups: [], users: [] };
+groups.forEach((group) => {
+  group.description = faker.lorem.sentences();
+});
 
-  // Create 10 groups
-  for (let id = 0; id < groupsCount; id++) {
-    data.groups.push({
-      id: id,
-      name: faker.commerce.department(),
-      color: faker.commerce.color(),
-      description: faker.lorem.sentences(2)
-    });
-  }
+// Create 1000 users
+const users = Array
+  // We are going to use only a subset of this
+  .from({ length: 1000 }, () => faker.helpers.contextualCard())
+  .map((user, index) => ({
+    id: index,
+    groupId: faker.random.number({ min: 0, max: groups.length - 1 }),
+    name: user.name,
+    email: user.email,
+    avatar: user.avatar,
+  }))
 
-  // Create 1000 users
-  for (let id = 0; id < usersCount; id++) {
-    data.users.push({
-      id: id,
-      groupId: faker.random.number({ min: 0, max: groupsCount }),
-      ...faker.helpers.contextualCard()
-    });
-  }
-
-  return data
-}
+module.exports = () => ({ groups, users });
